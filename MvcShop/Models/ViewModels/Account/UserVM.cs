6bbb08cf4dba587ed0c0f1b3bbc350ace7e53e19
Danila <belkin.job123@gmail.com>
2021-09-25 14:18:48 +0300

    Added User Table and her models

---
+++ MvcShop/Models/ViewModels/Account/UserVM.cs
@@ -0,0 +1,39 @@
+﻿using MvcShop.Models.Data;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MvcShop.Models.ViewModels.Account
+{
+    public class UserVM
+    {
+        public UserVM() { }
+        public UserVM(UserDTO row)
+        {
+            Id = row.Id;
+            Username = row.Username;
+            EmailAdress = row.EmailAdress;
+            Password = row.Password;
+            FirstName = row.FirstName;
+            LastName = row.LastName;
+        }
+
+        public int Id { get; set; }
+        [Required]
+        public string Username { get; set; }
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        public string EmailAdress { get; set; }
+        [Required]
+        public string Password { get; set; }
+        [Required]
+        public string ConfirmPassword { get; set; }
+        [Required]
+        public string FirstName { get; set; }
+        [Required]
+        public string LastName { get; set; }
+    
+    }   
+}
